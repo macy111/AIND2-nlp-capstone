4c8ae5a14afac5fc917eb2c6c9c5dcabc10004d5
macy <2449236056@qq.com>
2017-08-17 22:54:10 +0800

    add all

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 
 
 def load_data(path):
@@ -6,7 +7,7 @@ def load_data(path):
     Load dataset
     """
     input_file = os.path.join(path)
-    with open(input_file, "r") as f:
+    with codecs.open(input_file, "r","utf-8") as f:
         data = f.read()
 
     return data.split('\n')
